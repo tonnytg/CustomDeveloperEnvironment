cbb35e42ab91626b9dbfb24270a49e68d11d139d
Antonio Thomacelli <tonnytg@gmail.com>
2021-07-02 08:27:42 -0300

    fix: ajust resource bucket

--- Terraform/RemoteState/variables.tf
+++ Terraform/RemoteState/variables.tf
@@ -1,11 +1,5 @@
-variable "project_id" {
-  type        = string
-  description = "Project to storage terraform state"
-  default     = "your-project"
-}
+variable "project_id" {}
 
-variable "region" {
-  type        = string
-  description = "Region of project"
-  default     = "your-region"
-}
+variable "region" {}
+
+variable "account"  {}
