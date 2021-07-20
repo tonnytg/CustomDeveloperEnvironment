a4a2d50fd5dd7efc8c38390636f4acff48cd2129
Antonio Thomacelli <tonnytg@gmail.com>
2021-07-20 14:36:16 -0300

    feat: clean gke cluster

--- Terraform/src/deployment/gke_cluster.tf
+++ Terraform/src/deployment/gke_cluster.tf
@@ -1,11 +1,7 @@
+#https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_cluster
 data "google_service_account" "myaccount" {
   account_id = var.account
 }
-
-# resource "google_service_account" "default" {
-#   account_id   = "service-account-id"
-#   display_name = "Service Account"
-# }
 
 resource "google_container_cluster" "primary" {
   name               = "mycluster-main"
@@ -13,7 +9,7 @@ resource "google_container_cluster" "primary" {
   initial_node_count = 1
   node_config {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    # service_account = google_service_account.default.email
+    # https://cloud.google.com/kubernetes-engine/docs/how-to/iam
     service_account = data.google_service_account.myaccount.email
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
@@ -27,4 +23,8 @@ resource "google_container_cluster" "primary" {
     create = "30m"
     update = "40m"
   }
+}
+
+output "account_id" {
+  value = data.google_service_account.myaccount.email
 }
