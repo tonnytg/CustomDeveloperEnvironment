9125bff01c016bd51d09e85ad22846e3175d9edf
Antonio Thomacelli <tonnytg@gmail.com>
2021-07-20 16:09:21 -0300

    fix: add credentials

--- Terraform/src/deployment/provider.tf
+++ Terraform/src/deployment/provider.tf
@@ -1,11 +1,13 @@
 terraform {
   backend "gcs" {
-    bucket = "tf-backend-remote-eu-tf-backend-remote"
+    bucket = "eng-venture-320213"
     prefix = "terraform/state"
+    credentials = "~/.config/gcloud/eng-venture-320213-terraform-admin.json"
   }
 }
 
 provider "google" {
   project = var.project_id
   region  = var.region
+  credentials = file("~/.config/gcloud/eng-venture-320213-terraform-admin.json")
 }
